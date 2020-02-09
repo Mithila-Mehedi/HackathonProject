b07a340a117411ee9dab3bccdecf7dec908099ae
Mithila-Mehedi <mithilamehedi60@gmail.com>
2020-02-09 14:19:39 +0600

    int

--- app/src/main/java/com/example/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/hackathonproject/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_front_main);
+        setContentView(R.layout.activity_main);
 
         mAuth= FirebaseAuth.getInstance();
 
